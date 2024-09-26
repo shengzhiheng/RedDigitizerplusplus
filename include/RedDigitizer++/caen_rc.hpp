bcb195e3d25621aa367a12590a6e55a00f193ed6
Zhiheng Sheng <25388025+shengzhiheng@users.noreply.github.com>
2024-09-26 14:53:19 -0500

    start header file for wrapper of RedDigitizer++ for python .so import

---
+++ include/RedDigitizer++/caen_rc.hpp
@@ -0,0 +1,27 @@
+#ifndef CAEN_RC_HPP
+#define CAEN_RC_HPP
+
+// include red digitizer plus plus library
+#include "RedDigitizer++"
+
+// declare to be C compatible, so it can be imported into python
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// open digitizer
+
+// set configuration (global / group)
+
+// start acquisition
+
+// stop acquisition
+
+// retrieve data
+
+// close digitizer
+
+// close out extern C
+#ifdef __cplusplus
+}
+#endif
