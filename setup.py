db80d4b4562889db5192e7fdd7a7882a04075144
Zhiheng Sheng <25388025+shengzhiheng@users.noreply.github.com>
2025-03-07 18:47:24 -0600

    Change setup.py to skip dependencies when installing for readthedocs

--- setup.py
+++ setup.py
@@ -2,15 +2,20 @@
 from setuptools import setup, Extension
 import pybind11
 from pybind11.setup_helpers import Pybind11Extension
+import os
 
-ext_mods = [
-    Pybind11Extension(
-        "red_caen", 
-        ["red_digitizer_helper.cpp"], 
-        libraries=["CAENDigitizer"],
-        cxx_std=20
-    ),
-]
+# skip module import if compiling for ReadTheDocs
+if os.environ.get("READTHEDOCS"):
+    ext_mods = []
+else:
+    ext_mods = [
+        Pybind11Extension(
+            "red_caen", 
+            ["red_digitizer_helper.cpp"], 
+            libraries=["CAENDigitizer"],
+            cxx_std=20
+        ),
+    ]
 
 setup(
     name="red_caen",
